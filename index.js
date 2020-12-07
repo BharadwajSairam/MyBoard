d047e91d33ab0fc3012b866211eec911073fb91e
Bharadwaj Sairam <bharadwajsairam001@gmail.com>
2020-12-07 11:44:13 +0000

    server code generated

--- index.js
+++ index.js
@@ -4,3 +4,59 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const socket = require("socket.io");
+const { Socket } = require("dgram");
+const { static } = require("express");
+const express = require("express");
+const path = require("path");
+const app = express();
+const socket = require("socket.io");
+let socketlist=new Array();
+
+
+const PORT =process.env.PORT || 3000;
+
+const server = app.listen(PORT, ()=>{
+    console.log(`the server is running on port ${PORT}`)
+})
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+const io = socket(server);
+
+
+io.on("connection", (socket)=>{
+    socketlist.push(socket.id);
+    if (socketlist.length>1 ){
+    io.to(socketlist[0]).emit("get","data");}
+    socket.on("get", (data)=>{
+        console.log("server received get"+data);
+        console.log("new client "+socketlist[socketlist.length-1]);
+        io.to(socketlist[socketlist.length-1]).emit("init",data);
+    })
+
+    console.log(socket.id + "has just connected");
+
+    socket.on("hello", (data)=>{
+        console.log(data)
+    })
+
+    socket.on("draw", data=>{
+        socket.broadcast.emit("draw", data);
+    })
+
+    socket.on("erase", data=>{
+        socket.broadcast.emit("erase", data);
+    })
+    socket.on("unerase", data=>{
+        socket.broadcast.emit("unerase", data);
+    })
+    socket.on("clear", data=>{
+        socket.broadcast.emit("clear");
+    })
+    socket.on("save", data=>{
+        socket.broadcast.emit("clear");
+    })
+})
